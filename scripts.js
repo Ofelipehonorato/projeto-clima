b57df9b746d807310263588ccc1c5a4a03799194
Felipe Franciscato Honorato <50453449+Ofelipehonorato@users.noreply.github.com>
2022-06-27 17:05:53 -0300

    icone da semana

--- scripts.js
+++ scripts.js
@@ -17,47 +17,64 @@ setInterval (() =>{
     /* Dia da semana e o mês*/
     let dia = document.querySelector('.dia')
     let data = document.querySelector('.data')
-    let dayName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
+    let dayName = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
     let monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",  "Agosto", "Outubro", "Novembro", "Dezembro")
     let now = new Date
     dia.innerHTML = `<img src="./icons/calendario.png" width="30px">${dayName[now.getDay()]}, ${now.getDate()} de ${monName[now.getMonth()]}`
     //data.innerHTML = `${now.getDate()} de ${monName[now.getMonth()]}`
-    if (dayName[0]){
+    let diaDoDia
+switch (now.getDay()){
+    case 0:
+        diaDoDia = 'Domingo'
         document.querySelector('.domingo').style.backgroundColor = '#fca311'
         document.querySelector('.domingo').style.borderRadius = '50%'
         document.querySelector('.domingo').style.height = '30px'
         document.querySelector('.domingo').style.width = '30px'
-    } else if (dayName[1]){
+        break  
+    case 1:
+        diaDoDia = 'Segunda'
         document.querySelector('.segunda').style.backgroundColor = '#fca311'
         document.querySelector('.segunda').style.borderRadius = '50%'
         document.querySelector('.segunda').style.height = '30px'
         document.querySelector('.segunda').style.width = '30px'
-    } else if (dayName[2]){
+        break
+    case 2:
+        diaDoDia = 'Terça'
         document.querySelector('.terca').style.backgroundColor = '#fca311'
         document.querySelector('.terca').style.borderRadius = '50%'
         document.querySelector('.terca').style.height = '30px'
         document.querySelector('.terca').style.width = '30px'
-    } else if (dayName[3]){
+        break
+    case 3:
+        diaDoDia = 'Quarta'
         document.querySelector('.quarta').style.backgroundColor = '#fca311'
         document.querySelector('.quarta').style.borderRadius = '50%'
         document.querySelector('.quarta').style.height = '30px'
         document.querySelector('.quarta').style.width = '30px'
-    } else if (dayName[4]){
+        break
+    case 4:
+        diaDoDia = 'Quinta'
         document.querySelector('.quinta').style.backgroundColor = '#fca311'
         document.querySelector('.quinta').style.borderRadius = '50%'
         document.querySelector('.quinta').style.height = '30px'
         document.querySelector('.quinta').style.width = '30px'
-    } else if (dayName[5]){
+        break
+    case 5:
+        diaDoDia = 'Sexta'
         document.querySelector('.sexta').style.backgroundColor = '#fca311'
         document.querySelector('.sexta').style.borderRadius = '50%'
         document.querySelector('.sexta').style.height = '30px'
         document.querySelector('.sexta').style.width = '30px'
-    } else if (dayName[6]){
+        break
+    case 6:
+        diaDoDia = 'Sabado'
         document.querySelector('.sabado').style.backgroundColor = '#fca311'
         document.querySelector('.sabado').style.borderRadius = '50%'
         document.querySelector('.sabado').style.height = '30px'
         document.querySelector('.sabado').style.width = '30px'
-    }
+        break
+}
+
 
 /* Consulta da localização  */
 window.addEventListener('load', () => {
